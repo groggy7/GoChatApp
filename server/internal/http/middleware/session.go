d2c34a07efb726e5a068e8ac8ce14319bb9dc02d
Serhat Kılbaş <serhat.kilbas@priviasecurity.com>
2024-01-02 21:59:27 +0300

    Fix session validation middleware
    Middleware is only applicable to login requests

--- server/internal/http/middleware/session.go
+++ server/internal/http/middleware/session.go
@@ -15,24 +15,25 @@ var client = service.GetClient()
 
 func GetSessionMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		username := ctx.Param("username")
-		log.Println("in session middleware")
-		log.Println(username)
-		sessionJSON, err := client.Get(context.Background(), username).Result()
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			ctx.Abort()
-			return
-		}
+		if ctx.Request.URL.Path == "/login" {
+			username := ctx.Param("username")
+			log.Println("in session middleware")
+			log.Println(username)
+			sessionJSON, err := client.Get(context.Background(), username).Result()
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				ctx.Abort()
+				return
+			}
 
-		var session model.Session
-		if err := json.Unmarshal([]byte(sessionJSON), &session); err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			ctx.Abort()
-			return
+			var session model.Session
+			if err := json.Unmarshal([]byte(sessionJSON), &session); err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				ctx.Abort()
+				return
+			}
+			ctx.Set("session", &session)
 		}
-
-		ctx.Set("session", &session)
 		ctx.Next()
 	}
 }
