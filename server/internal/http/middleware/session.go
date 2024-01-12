2b197350cfc13f74390272d3bc1f75f50a42e2ad
Serhat Kılbaş <serhat.kilbas@priviasecurity.com>
2024-01-12 23:39:26 +0300

    Fix authorization middleware

--- server/internal/http/middleware/session.go
+++ server/internal/http/middleware/session.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"server/pkg/model"
 	"server/pkg/service"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,8 +15,14 @@ var client = service.GetClient()
 
 func GetSessionMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		if ctx.Request.URL.Path == "/login" {
+		if strings.Contains(ctx.Request.URL.Path, "/login") {
 			username := ctx.Param("username")
+			if username == "" {
+				ctx.JSON(http.StatusUnauthorized, gin.H{"error": "no username"})
+				ctx.Abort()
+				return
+			}
+
 			sessionJSON, err := client.Get(context.Background(), username).Result()
 			if err != nil {
 				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
